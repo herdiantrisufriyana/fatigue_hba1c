14051380c88f17e7d5e8b5664715d0b4f7ed1dcc
herdiantrisufriyana <herdiantrisufriyana@gmail.com>
2024-12-12 18:15:49 +0800

    Add strata argument.

--- R/reg_analysis_plot-function.R
+++ R/reg_analysis_plot-function.R
@@ -1,27 +1,77 @@
-reg_analysis_plot <- function(data){
+reg_analysis_plot <- function(data, strata = NULL){
+  
   data <-
     data |>
     mutate(variable = reorder(variable, UB)) |>
     arrange(variable) |>
     mutate(variable_num = as.numeric(variable))
   
-  data |>
-    ggplot(aes(variable_num, log(OR))) +
+  if(is.null(strata)){
+    p <-
+      data |>
+      ggplot(aes(variable_num, log(OR)))
+  }else{
+    variable_label <-
+      data.frame(
+        variable_num = unique(select(data, variable, variable_num))$variable_num
+        , label =
+          paste0(
+            unique(select(data, variable, variable_num))$variable
+            , ' - '
+            , max(data$variable_num)
+            - unique(select(data, variable, variable_num))$variable_num
+            + 1
+          )
+      )
+    
+    data <-
+      data |>
+      rename_at(strata, \(x) "strata") |>
+      left_join(variable_label,  by = join_by(variable_num)) |>
+      mutate_at("label", \(x) factor(x, rev(unique(x))))
+    
+    p <-
+      data |>
+      ggplot(aes(as.numeric(strata), log(OR), color = strata))
+  }
+  
+  p <-
+    p +
     geom_errorbar(aes(ymin = log(LB), ymax = log(UB)), width = 0.25) +
     geom_point() +
-    geom_hline(yintercept = log(1), color = "grey", linewidth = 1) +
-    coord_flip() +
-    scale_x_continuous(
-      breaks = unique(select(data, variable, variable_num))$variable_num
-      ,labels =
-        paste0(
-          unique(select(data, variable, variable_num))$variable
-          , ' - '
-          , max(data$variable_num)
-          - unique(select(data, variable, variable_num))$variable_num
-          + 1
-        )
-    ) +
+    geom_hline(yintercept = log(1), color = "grey", linewidth = 1)
+  
+  if(!is.null(strata)){
+    p <-
+      p +
+      facet_grid(label ~ ., switch = "y")
+  }
+  
+  p <-
+    p +
+    coord_flip()
+  
+  if(is.null(strata)){
+    p <- p +
+      scale_x_continuous(
+        breaks = unique(select(data, variable, variable_num))$variable_num
+        ,labels =
+          paste0(
+            unique(select(data, variable, variable_num))$variable
+            , ' - '
+            , max(data$variable_num)
+            - unique(select(data, variable, variable_num))$variable_num
+            + 1
+          )
+      )
+  }else{
+    p <-
+      p +
+      scale_x_reverse()
+  }
+  
+  p <-
+    p +
     theme_minimal() +
     xlab("") +
     scale_y_continuous(limits = c(log(1/60), log(60))) +
@@ -33,4 +83,17 @@ reg_analysis_plot <- function(data){
       , legend.position = "right"
       , legend.title = element_text(angle = 90, hjust = 0.5)
     )
+  
+  if(!is.null(strata)){
+    p <-
+      p +
+      theme(
+        axis.text.y = element_blank()
+        , strip.text.y.left = element_text(angle = 0, hjust = 1)
+        , strip.placement = "outside"
+        , legend.title = element_blank()
+      )
+  }
+  
+  p
 }
